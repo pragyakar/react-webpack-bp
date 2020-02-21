9ef7022b6f0caf3edccf0493b551315e3353c52d
pragyakarfuse <pragyakar@fusemachines.com>
2020-02-21 11:15:39 +0545

    Updated environment setup and run scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
-const path = require("path");
-const getMode = require("./webpack/mode");
-const getPlugins = require("./webpack/plugins");
-const getModules = require("./webpack/modules");
-const getResolve = require("./webpack/resolve");
-const getEntryFile = require("./webpack/entry");
-const PUBLIC_PATH = require("./webpack/constants").PUBLIC_PATH;
+const path = require('path');
+const getMode = require('./webpack/mode');
+const getPlugins = require('./webpack/plugins');
+const getModules = require('./webpack/modules');
+const getResolve = require('./webpack/resolve');
+const getEntryFile = require('./webpack/entry');
+const PUBLIC_PATH = require('./webpack/constants').PUBLIC_PATH;
 
-module.exports = (env = {}) => {
-  let { environment = "development", port = "9001" } = env;
-  const target = "web";
+const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+const port = process.env.PORT ? process.env.PORT : 9001;
+
+module.exports = () => {
   return {
     mode: getMode(environment),
     entry: getEntryFile(),
     output: {
       filename: (() => {
-        if (environment === "production" || environment === "uat") {
-          return "[name].[hash].js";
+        if (environment === 'production' || environment === 'uat') {
+          return '[name].[hash].js';
         } else {
-          return "[name].js";
+          return '[name].js';
         }
       })(),
-      chunkFilename: "[name].bundle.[hash].js",
-      path: path.resolve(__dirname, "dist"),
+      chunkFilename: '[name].bundle.[hash].js',
+      path: path.resolve(__dirname, 'dist'),
       publicPath: PUBLIC_PATH
     },
     devServer: {
@@ -29,13 +30,13 @@ module.exports = (env = {}) => {
       publicPath: PUBLIC_PATH,
       port: port,
       historyApiFallback: true,
-      host: "0.0.0.0"
+      host: '0.0.0.0'
     },
     module: getModules(),
     resolve: getResolve(),
-    plugins: getPlugins(environment, target),
+    plugins: getPlugins(environment),
     watchOptions: {
-      ignored: ["node_modules", "dist"],
+      ignored: ['node_modules', 'dist'],
       poll: true
     }
   };
--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,30 +1,29 @@
-const webpack = require("webpack");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
-const dotEnv = require("../env/dot-env");
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const dotEnv = require('../env/dot-env');
 
-module.exports = function generateWebpackPlugins(environment, target) {
+module.exports = function generateWebpackPlugins(environment) {
   const plugins = [
     new HTMLWebpackPlugin({
-      template: "./index.html" 
+      template: './index.html'
     }),
     new webpack.DefinePlugin({
       environment: JSON.stringify(environment),
-      envConfig: JSON.stringify(dotEnv.getEnvConfig(environment)),
-      target: JSON.stringify(target)
+      envConfig: JSON.stringify(dotEnv.getEnvConfig(environment))
     }),
     new ExtractTextPlugin({
-      filename: "public/assets/stylesheets/styles.min.css",
-      publicPath: "/public/assets/stylesheets/"
+      filename: 'public/assets/stylesheets/styles.min.css',
+      publicPath: '/public/assets/stylesheets/'
     })
   ];
-  if (environment === "production" || environment === "uat") {
+  if (environment === 'production' || environment === 'uat') {
     plugins.push(
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       new CompressionPlugin({
         test: /\.js(\?.*)?$/i,
-        algorithm: "gzip"
+        algorithm: 'gzip'
       })
     );
   }
