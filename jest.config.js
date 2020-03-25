ede7de0f02df1185dcb011a613f258e111349888
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-03-25 10:18:44 +0545

    added jest setup

--- jest.config.js
+++ jest.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  // globalSetup: undefined,
+  // globalSetup: "<rootDir>/src/setupTests.js",
 
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: undefined,
@@ -73,11 +73,13 @@ module.exports = {
     'jsx',
     'ts',
     'tsx'
-    //   "node"
+    // "node"
   ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -113,13 +115,13 @@ module.exports = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: ['<rootDir>/src']
+  roots: ['<rootDir>/src'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ['<rootDir>/src/setupTests.js']
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
---
+++ src/setupTests.js
@@ -0,0 +1,10 @@
+const dotEnv = require('../env/dot-env');
+
+const initEnvironmentVariables = () => {
+  return dotEnv.getEnvConfig(process.env.NODE_ENV);
+};
+
+(() => {
+  const envConfig = initEnvironmentVariables();
+  global.envConfig = envConfig;
+})();
