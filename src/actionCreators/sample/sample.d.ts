69704704865c878ac9f10ee8da2105e5809a572d
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-03-25 14:33:29 +0545

    added fill sample action creator

--- src/actionCreators/sample/sample.d.ts
+++ src/actionCreators/sample/sample.d.ts
@@ -1,7 +1,13 @@
 import * as actions from 'src/constants/actions';
 import { ActionCallbacks } from 'src/actionCreators/action';
+import { IFillSampleActionPayload } from './sample';
 
 export interface fetchSampleAction extends ActionCallbacks {
   type: typeof actions.SAMPLE_ACTION;
   payload: string;
+}
+
+export interface fillSampleAction extends ActionCallbacks {
+  type: typeof actions.FILL_SAMPLE_ACTION;
+  payload: IFillSampleActionPayload;
 }
